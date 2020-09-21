859185ae240d5a4cc48e41ee79ca36fb5eabd22c
Bruno <bruno.adamm@gmail.com>
2020-09-20 23:20:51 -0300

    loading screen UI improved with spin kit library

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:worldtimeapp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -32,10 +33,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text('Loading'),
+      backgroundColor: Colors.white,
+      body: Center(
+      child: SpinKitWave(
+        color: Colors.black,
+        size: 80.0,
       ),
+      )
     );
   }
 }
